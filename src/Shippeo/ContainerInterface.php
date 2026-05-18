e7f3e6ea13f4d4a0674c08e2b7f231e42d59bb3d
Pascal Paulis <ppaulis@gmail.com>
2026-05-18 09:34:24 +0200

    bugfix in date handling
    Signed-off-by: Pascal Paulis <ppaulis@gmail.com>

--- src/Shippeo/ContainerInterface.php
+++ src/Shippeo/ContainerInterface.php
@@ -2,71 +2,25 @@
 
 namespace BayWaReLusy\Shippeo;
 
+use DateTime;
+
 interface ContainerInterface
 {
-    /**
-     * @return string
-     */
     public function getId(): string;
-
-    /**
-     * @param string $id
-     * @return static
-     */
     public function setId(string $id): static;
 
-    /**
-     * @return string
-     */
     public function getUnLoCodeLoadingPort(): string;
-
-    /**
-     * @param string $unLoCodeLoadingPort
-     * @return static
-     */
     public function setUnLoCodeLoadingPort(string $unLoCodeLoadingPort): static;
 
-    /**
-     * @return string
-     */
     public function getUnLoCodeDestinationPort(): string;
-
-    /**
-     * @param string $unLoCodeDestinationPort
-     * @return static
-     */
     public function setUnLoCodeDestinationPort(string $unLoCodeDestinationPort): static;
 
-    /**
-     * @return string
-     */
     public function getMasterBillOfLadingReference(): string;
-
-    /**
-     * @param string $masterBillOfLadingReference
-     * @return static
-     */
     public function setMasterBillOfLadingReference(string $masterBillOfLadingReference): static;
 
-    /**
-     * @return string
-     */
     public function getOceanCarrierCode(): string;
-
-    /**
-     * @param string $oceanCarrierCode
-     * @return static
-     */
     public function setOceanCarrierCode(string $oceanCarrierCode): static;
 
-    /**
-     * @return \DateTime|null
-     */
-    public function getLastPushToShippeo(): ?\DateTime;
-
-    /**
-     * @param \DateTime|null $lastPushToShippeo
-     * @return static
-     */
-    public function setLastPushToShippeo(?\DateTime $lastPushToShippeo): static;
+    public function getLastPushToShippeo(): ?DateTime;
+    public function setLastPushToShippeo(?DateTime $lastPushToShippeo): static;
 }
